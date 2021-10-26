3de97bb8e2979848aa5e015adeca47f8558a03b9
Koo Hyong Mo <40062271+chrisais9@users.noreply.github.com>
2021-10-26 19:59:36 +0900

    1477 휴게소 세우기 [재채점 re-solve]

--- BOJ/휴게소 세우기/휴게소 세우기/main.cpp
+++ BOJ/휴게소 세우기/휴게소 세우기/main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n,m;
-int tb[500];
-int isAvail(int k)
-{
-    int i,cnt=0;
-    for(i=2;i<=n+2;++i)
-    {
-        if(tb[i]-tb[i-1]>k) cnt+=(tb[i]-tb[i-1]-1)/k;
-    }
-    if(cnt<=m)return 1;
-    return 0;
-}
-int main()
-{
-    int i;
-    cin>>n>>m>>tb[n+2];
-    for(i=2;i<=n+1;++i)cin>>tb[i];
-    sort(tb,tb+n+2);
-    int left=0,right=99999999,mid;
-    while(left<right)
-    {
-        mid=(left+right)/2;
-        if(isAvail(mid))right=mid;
-        else left=mid+1;
-    }
-    cout<<left;
+int n,m,l;
+int tb[110];
+
+int main() {
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+	cin>>n>>m>>l;
+	for (int i=1;i<=n;++i) {
+		cin>>tb[i];
+	}
+	tb[n+1]=l;
+	sort(tb, tb+n+1);
+	int left=1,right=l-1;
+	while (left<=right) {
+		int cnt = 0;
+		int mid = (left+right)/2;
+		for (int i=1;i<=n+1;++i) {
+			if (tb[i]-tb[i-1]> mid) {
+				cnt+=(tb[i]-tb[i-1]-1)/mid;
+			}
+		}
+		if (cnt>m) left=mid+1;
+		else right=mid-1;
+	}
+	cout<<left<< endl;
 }
