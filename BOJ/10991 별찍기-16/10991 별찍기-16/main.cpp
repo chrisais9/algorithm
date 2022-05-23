75f081180f70af89b8f333d6aaf6f4af3a4fba3a
chrisais9 <chrisais9@naver.com>
2022-05-23 16:02:40 +0900

    10991 별찍기-16

---
+++ BOJ/10991 별찍기-16/10991 별찍기-16/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++) {
+        for(int j=0;j<n-i;j++) {
+            cout << " ";
+        }
+        for(int j=0;j<=i*2-1;j++) {
+            if(j&1) cout<<" ";
+            else cout<< "*";
+        }
+        cout<<endl;
+    }
+}
